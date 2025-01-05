a3b8603c35256486254bc8704613d3e170e7b621
Fikiyoyo <159613220+Fikiyoyo@users.noreply.github.com>
2025-01-05 23:13:53 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,28 +1,28 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
-  BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: '', // put your session id between the ''
+  OWNER_NUMBERS: "2348077549692", // your number with country code
+  OWNER_NAME: 'fikiyoyo', // your owner name 
+  BOT_NAME: "Champ", // your preferred bot name
+  SESSION_ID: 'kord_ai-bzQvdAh8eUfi', // put your session id between the ''
   INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: "Public",
   TIME_ZONE: "Africa/Lagos",
   ANTI_VIEWONCE: true,
   READ_ALL_MESSAGES: false,
   AUTO_READ_STATUS: true,
-  PREFIX: [".", "/", "!"],
+  PREFIX: ["!"],
   WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
   DEFAULT_TRANSLATION_LANG: "en",
   COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
   MAX_COMMANDS_PER_MINUTE: 100,
   MAX_DOWNLOAD_SIZE: 100,
-  AUTO_LIKE_STATUS: true,
-  AUTO_LIKE_EMOJI: '🍁',
+  AUTO_LIKE_STATUS: false,
+  AUTO_LIKE_EMOJI: '',
   BANKING_DETAILS: {
-    bank: "Kord-Ai Bank", // the name of your preferred bank
-    acc_number: "1234567890", // account number for your preferred bank
-    acc_name: "MyAccountName" // name of your bank
+    bank: "Opay Bank", // the name of your preferred bank
+    acc_number: "8077549692", // account number for your preferred bank
+    acc_name: "ojo" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
